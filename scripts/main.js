7a721fa28b636ba5211284c8cefc3f464ef14394
cssmonkey <web@sean-jones.co.uk>
2016-08-05 15:10:50 +0100

    Animations added to order component

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var CSSTransitionGroup = require('react-addons-css-transition-group');
 
 var ReactRouter =  require('react-router');
 var Router = ReactRouter.Router;
@@ -186,10 +187,13 @@ var Order = React.createClass({
 
       return (
           <li key={key}>
-            {count}lbs
-            {fish.name}
-            <span className="price">{helpers.formatPrice(count * fish.price)}</span>
-            {removeBtn}
+            <span>
+                <CSSTransitionGroup component="span" transitionName="count" transitionEnterTimeout={250} transitionLeaveTimeout={250}>
+                    <span key={count}>{count}</span>
+                </CSSTransitionGroup>
+                lbs {fish.name} {removeBtn}
+            </span>
+            <span className="price">{helpers.formatPrice(count * fish.price)}</span> 
           </li>
       )
   },
@@ -209,13 +213,13 @@ var Order = React.createClass({
     return (
       <div className="order-wrap">
         <h2 className="order-title">Your order</h2>
-        <ul className="order">
+        <CSSTransitionGroup className="order" component="ul" transitionName="order" transitionEnterTimeout={500} transitionLeaveTimeout={500}>
             {orderIds.map(this.renderOrder)}
             <li className="total">
                 <strong>Total:</strong>
                 {helpers.formatPrice(total)}
             </li>
-        </ul>
+        </CSSTransitionGroup>
       </div>
     )
   }
